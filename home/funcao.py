aefe748779d84f85b4e5b02fb2c9d75db5aae6c9
Livio Azevedo <azevedols.livio@gmail.com>
2019-05-31 13:12:30 -0300

    Commit python funcao

---
+++ home/funcao.py
@@ -0,0 +1,8 @@
+def velocidade_media(distancia,tempo):
+	velocidade = distancia/tempo
+	print(velocidade)
+
+velocidade_media(100,20)
+velocidade_media(150,22)
+velocidade_media(200,30)
+velocidade_media(50,3)
