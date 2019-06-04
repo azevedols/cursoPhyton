1e07f3e6dcc60c6b4e3ffe4da386a6966c0008fe
Livio Azevedo <azevedols.livio@gmail.com>
2019-06-04 15:00:07 -0300

    Commit atualizacao curso python caelum

--- home/jogos/adivinhacao.py
+++ home/jogos/adivinhacao.py
@@ -1,106 +1,108 @@
 from random import randint
 
-print('******************************')
-print('*	Jogo da adivinhacao	*')
-print('******************************')
+def jogar():
 
-print('O Número secreto vai de 0 a 100.')
-print('O jogo possui 3 níveis de dificuldade\nNível 1 (Você tem 7 tentativas)\nNível 2 (Você tem 5 tentativas)\nNível 3 (Você tem 3 tentativas)\n')
+	print('******************************')
+	print('*	Jogo da adivinhacao	*')
+	print('******************************')
 
-nivel = int(input('Informe seu nível: '))
-numero_secreto = randint(0,100)
-PONTUACAO = 1000
+	print('O Número secreto vai de 0 a 100.')
+	print('O jogo possui 3 níveis de dificuldade\nNível 1 (Você tem 7 tentativas)\nNível 2 (Você tem 5 tentativas)\nNível 3 (Você tem 3 tentativas)\n')
 
-if(nivel == 1):
-	total_de_tentativas = 7
-	pontuacao_atual = 0
+	nivel = int(input('Informe seu nível: '))
+	numero_secreto = randint(0,100)
+	PONTUACAO = 1000
 
-	for rodada in range(1, total_de_tentativas + 1):
-		print('\nTentativa {} de {}' .format(rodada, total_de_tentativas))
+	if(nivel == 1):
+		total_de_tentativas = 7
+		pontuacao_atual = 0
 
-		chute = int(input('Digite o seu numero: \n'))
-		print('\nVoce digitou: ', chute)
+		for rodada in range(1, total_de_tentativas + 1):
+			print('\nTentativa {} de {}' .format(rodada, total_de_tentativas))
 
-		acertou = chute == numero_secreto
-		maior = chute > numero_secreto
-		menor = chute < numero_secreto
+			chute = int(input('Digite o seu numero: \n'))
+			print('\nVoce digitou: ', chute)
 
-		if (acertou):
-			print('\nVoce acertou!')
-			break
-		elif(maior):
-			print('\nVoce errou! \n O seu chute foi maior que o numero secreto.\n')
-		elif(menor):
-			print('\nVoce errou! \n O seu chute foi menor que o numero secreto.')
+			acertou = chute == numero_secreto
+			maior = chute > numero_secreto
+			menor = chute < numero_secreto
 
-		pontuacao_atual = abs(pontuacao_atual - chute) 
-		rodada = rodada + 1
+			if (acertou):
+				print('\nVoce acertou!')
+				break
+			elif(maior):
+				print('\nVoce errou! \n O seu chute foi maior que o numero secreto.\n')
+			elif(menor):
+				print('\nVoce errou! \n O seu chute foi menor que o numero secreto.')
 
-	pontuacao_atual = PONTUACAO - numero_secreto - (pontuacao_atual * 5)
-	print('Sua pontuação foi: {}'.format(pontuacao_atual))
-	print('O número secreto é: {}'.format(numero_secreto))
+			pontuacao_atual = abs(pontuacao_atual - chute) 
+			rodada = rodada + 1
 
-elif(nivel == 2):
-	total_de_tentativas = 5
-	pontuacao_atual = 0 
+		pontuacao_atual = PONTUACAO - numero_secreto - (pontuacao_atual * 5)
+		print('Sua pontuação foi: {}'.format(pontuacao_atual))
+		print('O número secreto é: {}'.format(numero_secreto))
 
-	for rodada in range(1, total_de_tentativas + 1):
-		print('\nTentativa {} de {}' .format(rodada, total_de_tentativas))
+	elif(nivel == 2):
+		total_de_tentativas = 5
+		pontuacao_atual = 0 
 
-
-		chute = int(input('Digite o seu numero: \n'))
-		print('\nVoce digitou: ', chute)
-
-		acertou = chute == numero_secreto
-		maior = chute > numero_secreto
-		menor = chute < numero_secreto
-
-		if (acertou):
-			print('\nVoce acertou!')
-			break
-		elif(maior):
-			print('\nVoce errou! \n O seu chute foi maior que o numero secreto.\n')
-		elif(menor):
-			print('\nVoce errou! \n O seu chute foi menor que o numero secreto.')
-
-		pontuacao_atual = abs(pontuacao_atual - chute)
-		rodada = rodada + 1
-
-	pontuacao_atual = PONTUACAO - numero_secreto - (pontuacao_atual * 3)
-	print('Sua pontuação foi: {}'.format(pontuacao_atual))
-	print('O número secreto é: {}'.format(numero_secreto))
-
-elif(nivel == 3):
-	total_de_tentativas = 3
-	pontuacao_atual = 0 
-
-	for rodada in range(1, total_de_tentativas + 1):
-		print('\nTentativa {} de {}' .format(rodada, total_de_tentativas))
+		for rodada in range(1, total_de_tentativas + 1):
+			print('\nTentativa {} de {}' .format(rodada, total_de_tentativas))
 
 
-		chute = int(input('Digite o seu numero: \n'))
-		print('\nVoce digitou: ', chute)
+			chute = int(input('Digite o seu numero: \n'))
+			print('\nVoce digitou: ', chute)
 
-		acertou = chute == numero_secreto
-		maior = chute > numero_secreto
-		menor = chute < numero_secreto
+			acertou = chute == numero_secreto
+			maior = chute > numero_secreto
+			menor = chute < numero_secreto
 
-		if (acertou):
-			print('\nVoce acertou!')
-			break
-		elif(maior):
-			print('\nVoce errou! \n O seu chute foi maior que o numero secreto.\n')
-		elif(menor):
-			print('\nVoce errou! \n O seu chute foi menor que o numero secreto.')
+			if (acertou):
+				print('\nVoce acertou!')
+				break
+			elif(maior):
+				print('\nVoce errou! \n O seu chute foi maior que o numero secreto.\n')
+			elif(menor):
+				print('\nVoce errou! \n O seu chute foi menor que o numero secreto.')
 
-		pontuacao_atual = abs(pontuacao_atual - chute)
-		rodada = rodada + 1
+			pontuacao_atual = abs(pontuacao_atual - chute)
+			rodada = rodada + 1
 
-	pontuacao_atual = PONTUACAO - numero_secreto - pontuacao_atual
-	print('Sua pontuação foi: {}'.format(pontuacao_atual))
-	print('O número secreto é: {}'.format(numero_secreto))
+		pontuacao_atual = PONTUACAO - numero_secreto - (pontuacao_atual * 3)
+		print('Sua pontuação foi: {}'.format(pontuacao_atual))
+		print('O número secreto é: {}'.format(numero_secreto))
 
-else:
-	print('Não existe esta alternativa\nTente novamente!!!')
+	elif(nivel == 3):
+		total_de_tentativas = 3
+		pontuacao_atual = 0 
 
-print('\nFim de jogo!')
+		for rodada in range(1, total_de_tentativas + 1):
+			print('\nTentativa {} de {}' .format(rodada, total_de_tentativas))
+
+
+			chute = int(input('Digite o seu numero: \n'))
+			print('\nVoce digitou: ', chute)
+
+			acertou = chute == numero_secreto
+			maior = chute > numero_secreto
+			menor = chute < numero_secreto
+
+			if (acertou):
+				print('\nVoce acertou!')
+				break
+			elif(maior):
+				print('\nVoce errou! \n O seu chute foi maior que o numero secreto.\n')
+			elif(menor):
+				print('\nVoce errou! \n O seu chute foi menor que o numero secreto.')
+
+			pontuacao_atual = abs(pontuacao_atual - chute)
+			rodada = rodada + 1
+
+		pontuacao_atual = PONTUACAO - numero_secreto - pontuacao_atual
+		print('Sua pontuação foi: {}'.format(pontuacao_atual))
+		print('O número secreto é: {}'.format(numero_secreto))
+
+	else:
+		print('Não existe esta alternativa\nTente novamente!!!')
+
+	print('\nFim de jogo!')
--- home/jogos/forca.py
+++ home/jogos/forca.py
@@ -1,43 +1,44 @@
-print('******************************')
-print('**Bem vindo ao jogo da Forca**')
-print('******************************')
+def jogar():
+	print('******************************')
+	print('**Bem vindo ao jogo da Forca**')
+	print('******************************')
 
-palavara_secreta = 'banana'
+	palavara_secreta = 'banana'
 
-letras_acertadas = ['_','_','_','_','_','_']
-print('\n')
+	letras_acertadas = ['_','_','_','_','_','_']
+	print('\n')
 
-acertou = False
-enforcou = False
+	acertou = False
+	enforcou = False
 
-erros = 0
-
-print(letras_acertadas)
-
-while(not acertou and not enforcou):
-	chute = input('\nDigite uma letra: ')
-
-	if(chute in palavara_secreta):
-		posicao = 0
-
-		for letra in palavara_secreta:
-			if (chute.upper() == letra.upper()):
-				letras_acertadas[posicao] = letra
-				print('\n')
-				
-			posicao += 1	
-			
-	else:
-		erros += 1	
-
-	acertou = '_' not in letras_acertadas
-	enforcou = erros == 6
+	erros = 0
 
 	print(letras_acertadas)
 
-if(acertou):
-	print('Você ganhou!!!')
-else:
-	print('Você perdeu!!!')
+	while(not acertou and not enforcou):
+		chute = input('\nDigite uma letra: ')
 
-print('Fim do Jogo!!!')
+		if(chute in palavara_secreta):
+			posicao = 0
+
+			for letra in palavara_secreta:
+				if (chute.upper() == letra.upper()):
+					letras_acertadas[posicao] = letra
+					print('\n')
+					
+				posicao += 1	
+				
+		else:
+			erros += 1	
+
+		acertou = '_' not in letras_acertadas
+		enforcou = erros == 6
+
+		print(letras_acertadas)
+
+	if(acertou):
+		print('Você ganhou!!!')
+	else:
+		print('Você perdeu!!!')
+
+	print('Fim do Jogo!!!')
